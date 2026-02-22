5b6bef782ff47fdbc6188fb6b5c3d553a4431a57
TrungDinh15 <lqd.sik22.dinhthanhtrung@gmail.com>
2026-02-22 15:33:33 +0700

    Shopee Web

--- src/core_app/ShopeeWeb/src/java/dal/QrMemory.java
+++
@@ -1,9 +0,0 @@
-package dal;
-
-import java.util.concurrent.ConcurrentHashMap;
-
-public class QrMemory {
-
-    // Lưu trữ: Token -> Tên User (Nếu chưa quét thì null)
-    public static ConcurrentHashMap<String, String> tokens = new ConcurrentHashMap<>();
-}
